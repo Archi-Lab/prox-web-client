1274050ca1ccd74e3693cae67618f7b439e0192e
cpolocze <40211062+cpolocze@users.noreply.github.com>
2019-12-02 04:24:59 +0100

    Add new queries to proposal-service

--- src/app/core/services/proposal.service.ts
+++ src/app/core/services/proposal.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { RestService } from 'angular4-hal';
 import { Proposal } from '../../shared/hal-resources/proposal.resource';
 import { Observable } from 'angular4-hal/node_modules/rxjs';
+import { UUID } from 'angular2-uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,29 @@ export class ProposalService extends RestService<Proposal> {
     super(Proposal, 'proposals', injector);
   }
 
-  findByProjectId(projectId: string): Observable<Proposal[]> {
+  findByProjectId(projectId: UUID): Observable<Proposal[]> {
     const options: any = { params: [{ key: 'projectId', value: projectId }] };
     return this.search('findByProjectId', options);
+  }
+
+  findByProjectIdAndStudentId(
+    projectId: UUID,
+    studentId: UUID
+  ): Observable<Proposal[]> {
+    const options: any = {
+      params: [
+        { key: 'projectId', value: projectId },
+        { key: 'studentId', value: studentId }
+      ]
+    };
+    return this.search('findByProjectIdAndStudentId', options);
+  }
+
+  findPublishedProposals(projectId: UUID): Observable<Proposal[]> {
+    const options: any = { params: [{ key: 'projectId', value: projectId }] };
+    return this.search(
+      'findByProjectIdAndSupervisorPermitsPublishIsTrueAndStudentPermitsPublishIsTrue',
+      options
+    );
   }
 }
