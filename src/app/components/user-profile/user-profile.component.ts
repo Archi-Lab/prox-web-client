f17e5a8f0a6b1c3f4f6d073b4c706a49ac45c59d
Dburgart <49637084+Dburgart@users.noreply.github.com>
2019-12-09 15:51:56 +0100

    -less code in user-profile.component.html -added variables and methods to open and close the projektlists -also mad changes in the scss file.

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
   constructor() {}
+  liste1offen = false;
+  liste2offen = false;
+  liste3offen = false;
 
   ngOnInit() {}
+  onSelect1(): boolean {
+    return (this.liste1offen = !this.liste1offen);
+  }
+  onSelect2(): boolean {
+    return (this.liste2offen = !this.liste2offen);
+  }
+  onSelect3(): boolean {
+    return (this.liste3offen = !this.liste3offen);
+  }
 }
