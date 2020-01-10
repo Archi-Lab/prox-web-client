4d3b91cca6475016f420ec445e9d10e8fba8f86c
John-Bryan Spieker <John-Bryan.Spieker@t-online.de>
2020-01-10 22:39:13 +0100

    - kopieren der project-list-struktur

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../../core/services/project.service';
+import { Project } from '../../shared/hal-resources/project.resource';
+import { MatSelectChange } from '@angular/material/select';
+import { MatConfirmDialogComponent } from '../../shared/mat-confirm-dialog/mat-confirm-dialog.component';
+import { ProjectDialogComponent } from '../project-dialog/project-dialog.component';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,19 +11,123 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-  constructor() {}
-  list1open = true;
-  list2open = false;
-  list3open = false;
+  projects: Project[] = [];
+  filteredProjects: Project[] = [];
+  allStatus: string[] = [];
+  selectedStatus: string;
+  selectedName: string;
+  selectedSupervisorName: string;
+  hasPermission = false;
 
-  ngOnInit() {}
-  onSelect1(): boolean {
-    return (this.list1open = !this.list1open);
+  constructor(private projectService: ProjectService) {}
+
+  ngOnInit() {
+    this.filterProjectsBySupervisorName('Dozent');
   }
-  onSelect2(): boolean {
-    return (this.list2open = !this.list2open);
+
+  getAllProjects() {
+    this.projectService
+      .getAll()
+      .subscribe(
+        projects => (this.projects = projects),
+        error => console.log(error),
+        () => this.fillStatus(this.projects)
+      );
   }
-  onSelect3(): boolean {
-    return (this.list3open = !this.list3open);
+
+  statusFilter(status: string) {
+    this.projectService
+      .findByStatus(status)
+      .subscribe(projects => (this.projects = projects));
+  }
+
+  supervisorNameFilter(supervisorName: string) {
+    this.projectService
+      .findBySupervisorName(supervisorName)
+      .subscribe(projects => (this.projects = projects));
+  }
+
+  nameFilter(name: string) {
+    if (this.selectedStatus) {
+      this.projectService
+        .findByStatus(this.selectedStatus)
+        .subscribe(projects => this.filterProjects(projects, name));
+    } else {
+      this.projectService
+        .getAll()
+        .subscribe(projects => this.filterProjects(projects, name));
+    }
+  }
+
+  filterProjectsByStatus(event: MatSelectChange) {
+    const status = event.value;
+    if (status) {
+      this.selectedStatus = status;
+      if (this.selectedName) {
+        this.nameFilter(this.selectedName);
+      } else {
+        this.statusFilter(status);
+      }
+    } else {
+      this.selectedStatus = null;
+      if (this.selectedName) {
+        this.nameFilter(this.selectedName);
+      } else if (this.selectedSupervisorName) {
+        this.supervisorNameFilter(this.selectedSupervisorName);
+      } else {
+        this.getAllProjects();
+      }
+    }
+  }
+
+  filterProjectsByName(event: any) {
+    const name = event.target.value;
+    if (name) {
+      this.selectedName = name;
+      this.nameFilter(name);
+    } else {
+      this.selectedName = null;
+      if (this.selectedStatus) {
+        this.statusFilter(this.selectedStatus);
+      } else if (this.selectedSupervisorName) {
+        this.supervisorNameFilter(this.selectedSupervisorName);
+      } else {
+        this.getAllProjects();
+      }
+    }
+  }
+
+  filterProjectsBySupervisorName(event: any) {
+    const supervisorName = event.target.value;
+    if (supervisorName) {
+      this.selectedSupervisorName = supervisorName;
+      this.supervisorNameFilter(this.selectedSupervisorName);
+    } else {
+      this.selectedSupervisorName = null;
+      if (this.selectedStatus) {
+        this.statusFilter(this.selectedStatus);
+      } else if (this.selectedName) {
+        this.nameFilter(this.selectedName);
+      } else {
+        this.getAllProjects();
+      }
+    }
+  }
+
+  private fillStatus(projects: Project[]) {
+    projects.forEach(project => this.allStatus.push(project.status));
+    this.allStatus = this.allStatus.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+  }
+
+  private filterProjects(projects: Project[], name?: string) {
+    for (const project of projects as Project[]) {
+      if (project.name.toLowerCase().includes(name.toLowerCase())) {
+        this.filteredProjects.push(project);
+      }
+    }
+    this.projects = this.filteredProjects;
+    this.filteredProjects = [];
   }
 }
