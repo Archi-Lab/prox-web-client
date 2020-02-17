6f1c8b58730ddaec434d0808e23d325433509bcb
John-Bryan Spieker <John-Bryan.Spieker@t-online.de>
2020-02-17 13:49:46 +0100

    Methodenaufruf korrigiert

--- src/app/core/services/professor.service.ts
+++ src/app/core/services/professor.service.ts
@@ -22,6 +22,6 @@ export class ProfessorService extends RestService<Professor> {
     const options: any = {
       params: [{ key: 'name', value: name }]
     };
-    return this.search('findByNameNameContains', options);
+    return this.search('findByNameNameContainsIgnoreCase', options);
   }
 }
